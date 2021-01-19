300d34c950a4f4d615e08448a41b42d831e812cb
Aaron <aaron-po@hotmail.co.uk>
2021-01-19 11:29:32 -0600

    Can read and write to server

---
+++ Assets/GStreamerUnity/Scripts/GstCustomPlayer.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Runtime.InteropServices;	// For DllImport.
+using System;
+
+public class GstCustomPlayer:IGstPlayer  {
+	
+	
+	internal const string DllName = "GStreamerUnityPlugin";
+	
+	[DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
+	extern static private System.IntPtr mray_gst_createCustomVideoPlayer();
+	
+	[DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
+	extern static private void mray_gst_customPlayerSetPipeline(System.IntPtr p, [MarshalAs(UnmanagedType.LPStr)]string pipeline);
+	
+	[DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
+	extern static private bool mray_gst_customPlayerCreateStream(System.IntPtr p);
+	
+	[DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
+	extern static private void mray_gst_customPlayerGetFrameSize(System.IntPtr p, ref int w, ref int h);
+	
+	[DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
+	extern static private bool mray_gst_customPlayerGrabFrame(System.IntPtr p, ref int w, ref int h);
+	
+	[DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
+	extern static private void mray_gst_customPlayerBlitImage(System.IntPtr p, System.IntPtr _TextureNativePtr, int _UnityTextureWidth, int _UnityTextureHeight);
+	
+	
+	public Vector2 FrameSize
+	{
+		get
+		{
+			int w=0,h=0;
+			mray_gst_customPlayerGetFrameSize(m_Instance,ref w,ref h);
+			return new Vector2(w,h);
+		}
+	}
+	
+	public GstCustomPlayer()
+	{
+		m_Instance = mray_gst_createCustomVideoPlayer();	
+	}
+	
+	
+	public void SetPipeline(string pipeline)
+	{		
+		mray_gst_customPlayerSetPipeline (m_Instance, pipeline);
+	}
+	public bool CreateStream()
+	{		
+		return mray_gst_customPlayerCreateStream (m_Instance);
+	}
+	
+	public bool GrabFrame(out Vector2 frameSize)
+	{
+		int w=0,h=0;
+		if(mray_gst_customPlayerGrabFrame(m_Instance,ref w,ref h))
+		{
+			frameSize.x=w;
+			frameSize.y=h;
+			return true;
+		}
+		frameSize.x = frameSize.y = 0;
+		return false;
+	}
+	
+	public void BlitTexture( System.IntPtr _NativeTexturePtr, int _TextureWidth, int _TextureHeight )
+	{
+		if (_NativeTexturePtr == System.IntPtr.Zero) return;
+		
+		Vector2 sz = FrameSize;
+		if (_TextureWidth != sz.x || _TextureHeight != sz.y) return;	// For now, only works if the texture has the exact same size as the webview.
+		
+		mray_gst_customPlayerBlitImage(m_Instance, _NativeTexturePtr, _TextureWidth, _TextureHeight);	// We pass Unity's width and height values of the texture
+	}
+}
---
+++ Assets/Scripts/VideoStreamReader.cs
@@ -0,0 +1,103 @@
+﻿using UnityEngine;
+using System.Collections;
+ 
+using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.IO;
+using UnityEngine.UI;
+using System.Collections.Generic;
+public class VideoStreamReader : MonoBehaviour
+{
+    // texture that will load the bytes
+    private Texture2D tex;
+    private RawImage rawImage;
+
+     // receiving Thread
+    Thread receiveThread;
+    private TcpClient client = null;
+    private NetworkStream theStream = null;
+    // public
+    public String Host = "192.168.2.1";   //default local
+    public int port = 5600;           // define port 
+ 
+    // infos
+    public string lastReceivedUDPPacket="";
+    public string allReceivedUDPPackets=""; // clean up this from time to time!
+
+    void Awake(){
+        rawImage = GameObject.Find("VideoStream").GetComponent<RawImage>();
+        UnityThread.initUnityThread();
+
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        tex = new Texture2D(2, 2);
+        client = new TcpClient();
+        client.Connect(Host, port);
+
+        theStream = client.GetStream();
+     
+        receiveThread = new Thread(new ThreadStart(ReceiveData));
+        receiveThread.IsBackground = true;
+        receiveThread.Start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+     // receive thread
+    private void ReceiveData()
+    {
+        try { 			
+            Byte[] bytes = new Byte[1024];             
+            while (true) { 				
+                // Get a stream object for reading 				
+                using (NetworkStream stream = client.GetStream()) { 					
+                    int length; 					
+                    // Read incomming stream into byte arrary. 					
+                    while ((length = stream.Read(bytes, 0, bytes.Length)) != 0) { 						
+                        var data = new byte[length]; 						
+                        Array.Copy(bytes, 0, data, 0, length); 						
+                        // Convert byte array to string message. 						
+                        string serverMessage = Encoding.ASCII.GetString(data); 						
+                        Debug.Log("server message received as: " + serverMessage); 	
+                        UnityThread.executeInUpdate(() =>
+                        {
+                            if (data[0] == 255 && data[1] == 216 && data[2] == 255 && data[3] == 224 && data[4] == 0 && data[5] == 16 && data[6] == 74 && data[7] == 70 && data[8] == 73 && data[9] == 70)
+                            {
+                                //UnityEngine.Debug.Log("* texture *");
+                                tex.LoadImage(data);
+                                rawImage.material.mainTexture = tex;  
+
+                            }       
+                        
+                        });				
+                    } 				
+                } 			
+            }         
+        }         
+        catch (SocketException socketException) {             
+            Debug.Log("Socket exception: " + socketException);         
+            Debug.Log("Error trying to read..." );         
+        }  
+       
+    }
+
+
+
+    void OnDisable()
+    {
+        if ( receiveThread!= null)
+            receiveThread.Abort();
+
+        client.Close();
+    }
+}
